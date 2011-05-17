4b1d95850668f4bb219994c13b509626531db923
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-05-17 11:08:55 +0200

    Add legends to images

--- assignment-1/plot.r
+++ assignment-1/plot.r
@@ -1,6 +1,6 @@
 # Read CSV files
-eager <- read.csv(file="goldbach-eager.csv", header=FALSE)
-lazy <- read.csv(file="goldbach-lazy.csv", header=FALSE)
+eager <- read.csv(file="sort-eager.csv", header=FALSE)
+lazy <- read.csv(file="sort-lazy.csv", header=FALSE)
 
 totals <- c(eager$V2, lazy$V2)
 min <- min(totals)
@@ -8,5 +8,6 @@ max <- max(totals)
 
 pdf("benchmarks.pdf")
 plot(eager, type="n", xlab="Input size", ylab="Time (in s)", ylim=c(min, max))
+legend("topleft", c("eager", "lazy"), fill=c("red", "green"))
 points(eager, col="red")
 points(lazy, col="green")
